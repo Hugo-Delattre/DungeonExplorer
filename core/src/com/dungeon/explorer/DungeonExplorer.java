af4cade01202a78fc9a161cda2551a6543499df8
Nina Girard <nina.girard96@gmail.com>
2023-11-08 22:19:48 +0100

    base du projet

--- core/src/com/dungeon/explorer/DungeonExplorer.java
+++ core/src/com/dungeon/explorer/DungeonExplorer.java
@@ -1,26 +1,26 @@
 package com.dungeon.explorer;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.dungeon.explorer.Screens.PlayScreen;
 
-public class DungeonExplorer extends ApplicationAdapter {
-	SpriteBatch batch;
+public class DungeonExplorer extends Game {
+	public SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
