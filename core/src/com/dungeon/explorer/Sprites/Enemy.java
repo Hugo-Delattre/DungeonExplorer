442a7592dde5a0d6a053ed05009d6e808a60543c
Hugo Delattre <delattre.hugo.24@gmail.com>
2023-11-16 21:28:44 +0100

    feat: add enemy remaining counter

--- core/src/com/dungeon/explorer/Sprites/Enemy.java
+++ core/src/com/dungeon/explorer/Sprites/Enemy.java
@@ -22,11 +22,15 @@ public abstract class Enemy extends Sprite {
     protected float stateTime;
     protected Animation<TextureRegion> walkAnimation;
     protected Array<TextureRegion> frames;
+    
+    public static int instanceCount = 0;
 
     public Enemy(PlayScreen screen, float x, float y) {
         this.world = screen.getWorld();
         this.screen = screen;
         setPosition(x, y);
+        instanceCount++;
+        Gdx.app.log("Enemy", "Created. " + instanceCount + " instances.");
         defineEnemy();
         velocity = new Vector2(1, 0);
     }
@@ -34,14 +38,15 @@ public abstract class Enemy extends Sprite {
     protected abstract void defineEnemy();
     public void hit() {
         lifePoints--;
-        Gdx.app.log("LifePoints", "- "+ lifePoints);
+        // Gdx.app.log("LifePoints", "- "+ lifePoints);
         if (lifePoints <= 0) {
             setToDestroy = true;
         }
     }
-
-    public void update(float dt) {
-
+    
+    public void dispose() {
+        instanceCount--;
+        Gdx.app.log("Enemy", "Disposed. " + instanceCount + " instances left.");
     }
 
     public void reverseVelocity(boolean x, boolean y) {
